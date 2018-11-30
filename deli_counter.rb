3356f86c81575450c623809ed6aa5937e55fadaf
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-30 06:28:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line(katz_deli)
+   if katz_deli.any? == false
+    puts "The line is currently empty."
+  elsif 
+    katz_deli.each.with_index(1) do |name, count|
+      counter = 0
+      puts "The line is currently: #{count}. #{name}"
+      counter += 1
+    end
+  end
+end
